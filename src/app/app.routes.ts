c029aae8c2621e51de41c02b46fbd1e4c1c687d0
AlexeyFrens <leiloka46@gmail.com>
2025-10-31 20:44:57 -0300

    feat: Roteamento das Paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import {CatalogoCarros} from './pages/catalogo-carros/catalogo-carros';
+import {Home} from './pages/home/home';
+import {Servicos} from './pages/servicos/servicos';
+import {Contato} from './pages/contato/contato';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Luxcar - Home'
+  },
+  {
+    path: 'carros',
+    component: CatalogoCarros,
+    title: 'Luxcar - Catalogo Carros'
+  },
+  {
+    path: 'servicos',
+    component: Servicos,
+    title: 'Luxcar - Serviços'
+  },
+  {
+    path: 'contato',
+    component: Contato,
+    title: 'Luxcar - Contato'
+  }
+];
---
+++ src/app/pages/catalogo-carros/catalogo-carros.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo-carros',
+  imports: [],
+  templateUrl: './catalogo-carros.html',
+  styleUrl: './catalogo-carros.css'
+})
+export class CatalogoCarros {
+
+}
