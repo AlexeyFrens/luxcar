a6cd6397502ccfbebc3f00367eece2537085623d
AlexeyFrens <leiloka46@gmail.com>
2025-11-06 15:42:27 -0300

    fix: Video nao inicia ao carregar a pagina

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,35 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements AfterViewInit {
 
+  @ViewChild('heroVideo') heroVideo!: ElementRef<HTMLVideoElement>;
+
+  /*
+  * Usando essa função, estamos garantindo que o vídeo já foi
+  * carregado no DOM e podemos manipulá-lo
+  */
+  ngAfterViewInit() {
+    const video = this.heroVideo.nativeElement;
+
+    video.muted = true;
+
+    const tryPlay = () => {
+      video.play().catch(err => {
+        console.warn(err);
+      })
+    }
+
+    //Verifica se o vídeo carregou dados suficientes para iniciar
+    if(video.readyState >= 3) {
+      tryPlay()
+    }else {
+      /* Espera o evento que indica que o vídeo está pronto para
+      *  iniciar (mesmo não carregando tudo)
+      *
+      *  Depois tenta iniciar novamente
+      */
+      video.addEventListener('loadeddata', tryPlay, {once: true})
+    }
+  }
 }
