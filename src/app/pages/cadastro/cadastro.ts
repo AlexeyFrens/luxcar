f451c09adaa91f0e0aaa2a20f608fec197350799
Marcio Jose <junior227733@gmail.com>
2025-11-17 21:58:45 -0300

    feat(cadastro): adicionado validacao do cpf

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -3,17 +3,19 @@ import { Cliente } from '../../core/types/type';
 import { CadastroService } from '../../core/services/cadastro.service';
 import { FormsModule } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,CommonModule
   ],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css'
 })
 export class Cadastro implements OnInit {
+  cpfInvalido = false;
 
   titulo = 'Cadastro de Clientes';
 
@@ -46,20 +48,16 @@ export class Cadastro implements OnInit {
   }
 
   submeter() {
-    if (this.clienteId) {
-
-      this.service.editar(this.cliente).subscribe(() => {
-        this.router.navigate(['/clientes']);
-      });
-
-    } else {
-      // Inclusão
-      this.service.incluirCliente(this.cliente).subscribe(() => {
-        this.router.navigate(['/clientes']);
-      });
+    if (!this.validarCPF(this.cliente.cpf)) {
+      alert("CPF inválido! Por favor, revise.");
+      return;
     }
-  }
 
+    this.service.incluirCliente(this.cliente).subscribe(() => {
+      alert("Cliente cadastrado com sucesso!");
+      this.router.navigate(['/clientes']);
+    });
+  }
   cancelar() {
     this.router.navigate(['/clientes']);
   }
@@ -73,4 +71,42 @@ export class Cadastro implements OnInit {
     this.senhaVisivel = !this.senhaVisivel;
     input.type = this.senhaVisivel ? 'text' : 'password';
   }
+
+  validarCPF(cpf: string): boolean {
+
+    cpf = cpf.replace(/\D/g, ""); // remove tudo que não é número
+
+    if (cpf.length !== 11) return false;
+
+    // CPFs inválidos conhecidos (ex: 11111111111)
+    if (/^(\d)\1+$/.test(cpf)) return false;
+
+    let soma = 0;
+    let resto;
+
+    // 1º dígito
+    for (let i = 1; i <= 9; i++) {
+      soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+
+    if (resto !== parseInt(cpf.substring(9, 10))) return false;
+
+    // 2º dígito
+    soma = 0;
+    for (let i = 1; i <= 10; i++) {
+      soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+
+    return resto === parseInt(cpf.substring(10, 11));
+  }
+
+
+
+
 }
--- src/app/pages/clientes/clientes.ts
+++ src/app/pages/clientes/clientes.ts
@@ -60,10 +60,13 @@ export class Clientes implements OnInit {
   }
 
   salvarEdicao() {
-    this.service.editar(this.clienteEditando).subscribe(() => {
-      // Recarrega a lista corretamente
-      this.ngOnInit();
+    if (!this.validarCPF(this.clienteEditando.cpf)) {
+      alert("CPF inválido! Por favor, revise.");
+      return;
+    }
 
+    this.service.editar(this.clienteEditando).subscribe(() => {
+      this.ngOnInit();
       this.modalAberto = false;
     });
   }
@@ -79,4 +82,45 @@ export class Clientes implements OnInit {
     if (campo) campo.type = this.senhaVisivel ? 'text' : 'password';
   }
 
+
+
+  validarCPF(cpf: string): boolean {
+
+    cpf = cpf.replace(/\D/g, "");
+
+    if (cpf.length !== 11) return false;
+
+
+    if (/^(\d)\1+$/.test(cpf)) return false;
+
+    let soma = 0;
+    let resto;
+
+
+    for (let i = 1; i <= 9; i++)
+      soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+    if (resto !== parseInt(cpf.substring(9, 10))) return false;
+
+
+    soma = 0;
+    for (let i = 1; i <= 10; i++)
+      soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) resto = 0;
+
+    return resto === parseInt(cpf.substring(10, 11));
+  }
+
+
+
+
+
 }
+
+
+
+
